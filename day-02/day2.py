3361a137b846be99e61c6cc9d99a8dd8a53d3e08
chriswowd <chriswowd@gmail.com>
2022-12-21 00:19:01 +0530

    completed day-02

---
+++ day-02/day2.py
@@ -0,0 +1,11 @@
+nooftri=int(input())
+tris = [list(map(int,input().split())) for i in range(nooftri)]
+
+for i,tris in enumerate(tris):
+        tris.sort()
+        if i % 3 == 0:
+            print(tris[0])
+        elif i % 3 == 1:
+            print(tris[1])
+        else:
+            print(tris[2])
